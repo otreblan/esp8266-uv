02c5cdc9daed72fde04cb915d57703d684087e55
otreblan <otreblain@gmail.com>
2024-06-14 18:19:39 -0500

    First commit

---
+++ start-lsp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Run this script to generate a compile_commands.json for autocompletion.
+
+BUILD_PATH="build"
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B "$BUILD_PATH"
+ln -s "$BUILD_PATH/compile_commands.json" compile_commands.json
+
+# https://github.com/MaskRay/ccls/issues/931
+sed -ri 's/"[^"]*-(gcc|g++) /"\1 /' "$BUILD_PATH/compile_commands.json"
