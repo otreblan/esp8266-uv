6eadd3cd32d341ba898d06df502412b0261361db
otreblan <otreblain@gmail.com>
2024-06-15 07:39:26 -0500

    feat(esp8266): Add logging

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,37 +1,34 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 
 #include "driver/gpio.h"
 
+static const char* TAG = "uv";
+#define UV_LOGI(format, ... ) ESP_LOGI(TAG, format, ##__VA_ARGS__)
+
+void log_chip_info()
+{
+	UV_LOGI("Hello world!\n");
+
+	/* Print chip information */
+	esp_chip_info_t chip_info;
+	esp_chip_info(&chip_info);
+
+	UV_LOGI("This is ESP8266 chip with %d CPU cores, WiFi, ", chip_info.cores);
+	UV_LOGI("silicon revision %d, ", chip_info.revision);
+	UV_LOGI("%dMB %s flash\n", (int)spi_flash_get_chip_size() / (1024 * 1024),
+		(chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+}
+
 void app_main()
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", (int)spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
+	log_chip_info();
 
 	gpio_num_t GPIO_LED = GPIO_NUM_16;
-
 
 	gpio_config_t io_config =
 	{
