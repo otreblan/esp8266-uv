03b977cb1c391cba1752386d6e24a201b7695dcd
otreblan <otreblain@gmail.com>
2024-06-16 16:31:12 -0500

    feat(Sensors): Take analog input

--- main/uv_main.cpp
+++ main/uv_main.cpp
@@ -1,18 +1,15 @@
-#include "nvs_flash.h"
+#include "driver/adc.h"
 #include "driver/gpio.h"
+#include "nvs_flash.h"
 
 #include "mqtt.hpp"
 #include "utils.h"
 #include "wifi.hpp"
 
-void cpp_main()
+gpio_num_t GPIO_LED = GPIO_NUM_16;
+
+void start_gpios()
 {
-	log_chip_info();
-
-	ESP_ERROR_CHECK(nvs_flash_init());
-
-	gpio_num_t GPIO_LED = GPIO_NUM_16;
-
 	gpio_config_t io_config =
 	{
 		.pin_bit_mask = 1U << GPIO_LED,
@@ -24,7 +21,27 @@ void cpp_main()
 	};
 
 	gpio_config(&io_config);
+}
 
+void start_adc()
+{
+
+	adc_config_t adc_config = {
+		.mode    = ADC_READ_TOUT_MODE,
+		.clk_div = 8
+	};
+
+	ESP_ERROR_CHECK(adc_init(&adc_config));
+}
+
+void cpp_main()
+{
+	log_chip_info();
+
+	ESP_ERROR_CHECK(nvs_flash_init());
+
+	start_gpios();
+	start_adc();
 
 	uv::wifi        wifi("", "");
 	uv::mqtt_client mqtt_client("mqtt://10.42.0.1:1883");
@@ -35,8 +52,14 @@ void cpp_main()
 		{
 			UV_LOGI("Publish");
 
+			uint16_t data;
+			ESP_ERROR_CHECK(adc_read(&data));
+			UV_LOGI("Analog input: %d", data);
+
+			std::string publish_data = std::to_string(data);
+
 			gpio_set_level(GPIO_LED, 1);
-			mqtt_client.publish("/hello_world", nullptr, 0, 1);
+			mqtt_client.publish("/hello_world", publish_data.c_str(), publish_data.size(), 1);
 			gpio_set_level(GPIO_LED, 0);
 		}
 	}
